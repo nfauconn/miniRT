a816da6dcb3a7b8216eb3f471ed17212b8411594
miaou33 <nfauconn@student.42.fr>
2022-10-25 15:18:33 +0200

    libft

---
+++ libft/srcs/comparison_search/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 13:06:43 by nfauconn          #+#    #+#             */
+/*   Updated: 2022/10/25 15:13:25 by nfauconn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	to_find;
+	char	*s1;
+
+	to_find = (char)c;
+	s1 = (char *)s;
+	if (to_find == '\0')
+		return (&s1[ft_strlen(s1)]);
+	while (s1 && *s1)
+	{
+		if (*s1 == to_find)
+			return (s1);
+		s1++;
+	}
+	return (NULL);
+}
---
+++ libft/srcs/comparison_search/ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 13:55:46 by nfauconn          #+#    #+#             */
+/*   Updated: 2022/10/25 15:13:55 by nfauconn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (!n)
+		return (0);
+	i = 0;
+	while (s1[i] && s2[i] && (s1[i] == s2[i]) && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
