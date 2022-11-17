75480950a5af7384ff3295d883c42913a181e6a9
miaou33 <nfauconn@student.42.fr>
2022-11-17 17:23:45 +0100

    interesect with absolu formula

--- srcs/ray.c
+++ srcs/ray.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 13:22:19 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/11/17 15:36:54 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/11/17 17:13:37 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,17 +57,7 @@ t_point	position(t_ray ray, float t)
 	return (ray.orig + ray.dest * t);
 }
 
-t_intersect	set_xs(size_t count, float pos1, float pos2)
-{
-	t_intersect	xs;
-
-	xs.count = count;
-	xs.pos[0] = pos1;
-	xs.pos[1] = pos2;
-	return (xs);
-}
-
-// r.orig{0, 0, -5} + r.dir * t = {0, 0, -1} <=> r.orig.z + r.dir * t = sphere.z <=> r.dir * t = sphere.z - r.orig.z <=> t = (-1 - r.orig.z) / r.dir.z
+/* // r.orig{0, 0, -5} + r.dest * t = {0, 0, -1} <=> r.orig.z + r.dest * t = sphere.z <=> r.dest * t = sphere.z - r.orig.z <=> t = (-1 - r.orig.z) / r.dest.z
 t_intersect	intersect(t_sphere s, t_ray r)
 {
 	t_intersect	xs;
@@ -90,8 +80,8 @@ t_intersect	intersect(t_sphere s, t_ray r)
 		return (set_xs(0, 0, 0));
 	return (xs);
 }
-
-// r.orig{0, 1, -5} + r.dest.z * ? = tangente{0, 1, 0} <=> r.dir.z * ? = 0 - r.orig.z <=> ? = (0 -r.orig.z) / r.dir.z
+ */
+// r.orig{0, 1, -5} + r.dest.z * ? = tangente{0, 1, 0} <=> r.dest.z * ? = 0 - r.orig.z <=> ? = (0 -r.orig.z) / r.dest.z
 /* t_intersect	tangent_sp_intersect(t_sphere sphere, t_ray r)
 {
 	t_intersect	xs;
@@ -101,7 +91,54 @@ t_intersect	intersect(t_sphere s, t_ray r)
 	xs.pos[0] = -r.orig.z / r.dest.z;
 	xs.pos[1] = xs.pos[0];
 	return (xs);
-} */
+}
+	if ((r.orig.z < 0 && r.dest.z <= 0)
+		|| (r.orig.z > 0 && r.dest.z >= 0)
+		|| (r.dest.z == 0 && !same_tuple(r.orig, s.center)))
+		return (set_xs(0, 0, 0));
+	if (same_tuple(r.orig, s.center))
+		t = 0;
+	else
+		t = -r.orig.z / r.dest.z; //to find what is t for position.z = 0
+	pos = position(r, t);
+ */
+
+t_intersect	set_xs(float discriminant, float a, float b)
+{
+	t_intersect	xs;
+
+	if (discriminant < 0)
+	{
+		xs.count = 0;
+		xs.pos[0] = 0;
+		xs.pos[1] = 0;
+	}
+	else
+	{
+		xs.count = 2;
+		xs.pos[0] = (-b - sqrt(discriminant)) / (2 * a);
+		xs.pos[1] = (-b + sqrt(discriminant)) / (2 * a);
+	}
+	return (xs);
+}
+
+t_intersect	intersect(t_sphere s, t_ray r)
+{
+	t_intersect	xs;
+	t_vector	sphere_to_ray;
+	float		a;
+	float		b;
+	float		c;
+	float		discriminant;
+
+	sphere_to_ray = r.orig - s.center;
+	a = dot3(r.dest, r.dest);
+	b = 2 * dot3(r.dest, sphere_to_ray);
+	c = dot3(sphere_to_ray, sphere_to_ray) - 1;
+	discriminant = pow(b, 2) - 4 * a * c;
+	xs = set_xs(discriminant, a, b);
+	return (xs);
+}
 
 int	main()
 {
