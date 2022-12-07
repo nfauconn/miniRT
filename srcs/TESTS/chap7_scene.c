0817844bf9ca23c7442977cd8c234867002b1468
RoKerjea <rokerjea@student.42.fr>
2022-12-07 17:09:15 +0100

    all tests but not all functions for chapter7

--- srcs/TESTS/chap7_scene.c
+++ srcs/TESTS/chap7_scene.c
@@ -6,7 +6,7 @@
 /*   By: rokerjea <rokerjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:29:05 by rokerjea          #+#    #+#             */
-/*   Updated: 2022/12/04 20:54:19 by rokerjea         ###   ########.fr       */
+/*   Updated: 2022/12/07 17:09:10 by rokerjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,6 @@ t_xs	intersect_world(t_scene world, t_ray ray)
 	// 	res.t[0] = res.t[1];
 	return (res);
 }
-
 
 void	print_shape(t_elem *shape)
 {
@@ -192,9 +191,9 @@ Test(scene, shade_out)
 	t_scene	world;
 	setup_scene(&world, "./scenes/default.rt");
 	t_ray	r = ray(create_point(0, 0, -5), create_vector(0, 0, 1));
-	t_elem	shape = world.objs;
+	t_elem	shape = *world.objs;
 	t_inter	i = intersection(4, shape);
-	t_comps	cpmps = prepare_computations(i, r);
+	t_comps	comps = prepare_computations(i, r);
 
 	t_rgb	c = shade_hit(world, comps);
 
@@ -205,11 +204,11 @@ Test(scene, shade_in)
 {
 	t_scene	world;
 	setup_scene(&world, "./scenes/default.rt");
-	w.light = point_light(create_point(0, 0.25, 0), create_vector(255, 255, 255));
+	world.light = point_light(create_point(0, 0.25, 0), create_vector(255, 255, 255));
 	t_ray	r = ray(create_point(0, 0, -5), create_vector(0, 0, 1));
 	t_elem	shape = world.objs->next;
 	t_inter	i = intersection(0.5, shape);
-	t_comps	cpmps = prepare_computations(i, r);
+	t_comps	comps = prepare_computations(i, r);
 
 	t_rgb	c = shade_hit(world, comps);
 
@@ -243,14 +242,14 @@ Test(scene, ray_inside)
 	t_scene	world;
 	setup_scene(&world, "./scenes/default.rt");
 	t_elem	*outer = world.objs;
-	outer.material.ambiant = 1.0;
+	outer->material.ambient = 1.0;
 	t_elem	*inner = outer.next;
-	inner.material.ambiant = 1.0;
+	inner->material.ambient = 1.0;
 	t_ray	r = ray(create_point(0, 0, 0.75), create_vector(0, 0, -1));
 
 	t_rgb c = color_at(world, r);
 
-	cr_expect(same_tuple(c, inner.material.color));
+	cr_expect(same_tuple(c, inner->material.color));
 }
 
 t_m4x4_f	view_transform(t_point from, t_point to, t_point up)
@@ -262,7 +261,7 @@ t_m4x4_f	view_transform(t_point from, t_point to, t_point up)
 	t_vector	true_up;
 
 	forward = normalize(to - from);
-	upn = normalixe(up);
+	upn = normalize(up);
 	left = cross_product(forward, upn);
 	true_up = cross_product(left, forward);
 	res = identity_matr();
@@ -332,17 +331,36 @@ typedef	struct	s_camera
 	float		hsize;
 	float		vsize;
 	float		fov;
+	float		half_width;
+	float		half_height;
+	float		pixel_size;
 	t_m4x4_f	transform;
 }				t_camera;
 
+//aspect is the aspect ratio, like 16:9
 t_camera	setup_camera(float hsize, float vsize, float fov)
 {
 	t_camera	cam;
+	float		half_view;
+	float		aspect;
 
 	cam.hsize = hsize;
 	cam.vsize = vsize;
 	cam.fov = fov;
 	cam.transform = identity_matr();
+	half_view = tan(fov / 2);
+	aspect = cam.hsize / cam.vsize;
+	if (aspect >= 1)
+	{
+		cam.half_width = half_view;
+		cam.half_height = half_view / aspect;
+	}
+	else
+	{
+		cam.half_width = half_view * aspect;
+		cam.half_height = half_view;
+	}
+	cam.pixel_size = (cam.half_width * 2) / cam.hsize;
 	return (cam);
 }
 
@@ -371,4 +389,103 @@ Test(scene, camera_pixel_size)
 	c = setup_camera(125, 200, M_PI / 2);
 
 	cr_expect(c.pixel_size == 0.01);
+}
+
+t_ray	ray_for_pixel(t_camera cam, float px, float py)
+{
+	float	x_offset;
+	float	y_offset;
+	float	world_x;
+	float	world_y;
+
+	x_offset = (px + 0.5) * cam.pixel_size;
+	y_offset = (py + 0.5) * cam.pixel_size;
+	world_x = cam.half_width - x_offset;
+	world_y = cam.half_height - y_offset;
+
+	t_ray	res;
+	t_float4	pixel;
+	pixel = matrix_tuple_mult(inverse(cam.transform), create_point(world_x, world_y, -1));
+	res.orig = matrix_tuple_mult(inverse(cam.transform), create_point(0, 0, 0));
+	res.dest = normalize(pixel - res.orig);
+	return (res);
+}
+
+Test(scene, center_ray)
+{
+	t_camera	c;
+	c = setup_camera(201, 101, M_PI / 2);
+	t_ray	ray;
+	ray = ray_for_pixel(c, 100, 50);
+
+	cr_expect(same_tuple(ray.orig, create_point(0, 0, 0)));
+	cr_expect(same_tuple(ray.dest, create_vector(0, 0, -1)));
+}
+
+Test(scene, corner_ray)
+{
+	t_camera	c;
+	c = setup_camera(201, 101, M_PI / 2);
+	t_ray	ray;
+	ray = ray_for_pixel(c, 0, 0);
+
+	cr_expect(same_tuple(ray.orig, create_point(0, 0, 0)));
+	cr_expect(same_tuple(ray.dest, create_vector(0.66519, 0.33259, -0.66851)));
+}
+
+Test(scene, transformed_ray)
+{
+	t_camera	c;
+	c = setup_camera(201, 101, M_PI / 2);
+	t_ray	ray;
+	ray = ray_for_pixel(c, 100, 50);
+	c.transform = rotation_y(M_PI / 4) * translation(0, -2, 5);
+	cr_expect(same_tuple(ray.orig, create_point(0, 2, -5)));
+	cr_expect(same_tuple(ray.dest, create_vector(sqrt(2) / 2, 0, -(sqrt(2) / 2))));
+}
+
+//t_rgb ** as return type is probably a bad idea
+//because it would need to malloc a biiig amount of memory
+//but it would work as an idea and produce a tab of color, pixel by pixel
+t_rgb	**render(t_camera cam, t_scene world)
+{
+	int		x;
+	int		y;
+	t_ray	ray;
+	t_rgb	color;
+	t_rgb	**image;
+	//image could be the mlx image in th final version
+	//canvas doesn't even exist yet, it should be a kind of big malloc, i guess
+	image = canvas(cam.hsize, cam.vsize);
+	//it write color in the image, line by line, column by column
+	//depending from what the new ray touch in the world
+	y = 0;
+	while(y < cam.vsize - 1)
+	{
+		x = 0;
+		while (x < cam.hsize - 1)
+		{
+			ray = ray_for_pixel(cam, x, y);
+			color = color_at(world, ray);
+			//this should be replaced by my_mlx_pixelput
+			image[y][x] = color;
+			x++;
+		}
+		y++;
+	}
+	return (image);
+}
+
+Test(scene, full_scene)
+{
+	t_scene	w;
+	setup_scene(&w, "./scenes/default.rt");
+	t_camera	c;
+	c = setup_camera(11, 11, M_PI / 2);
+	t_float4	from = create_point(0, 0, -5);
+	t_float4	to = create_point(0, 0, 0);
+	t_float4	up = create_vector(0, 1, 0);
+	c.transform = view_transform(from, to, up);
+	image = render(c, w);
+	cr_expect(same_tuple(pixel_at(image, 5, 5), create_vector(0.38066, 0.47583, 0.2855)));
 }
