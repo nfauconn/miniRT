f90b16a9f6ba944ddd0ed5a74196a0ac2c2aaf5b
miaou33 <nfauconn@student.42.fr>
2022-12-11 20:08:39 +0100

    trying to drawscene

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,15 +6,15 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:58:49 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/12/11 19:13:31 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/12/11 19:51:29 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# define WIDTH 100
-# define HEIGHT 50
+# define WIDTH 300
+# define HEIGHT 150
 # define BLACK {0, 0, 0, 0}
 # define WHITE {1, 1, 1, 0}
 
