35e565043e11bb52455e7939ea0361933ea03589
miaou33 <nfauconn@student.42.fr>
2022-12-16 19:44:21 +0100

    plane

--- srcs/draw/drawscene.c
+++ srcs/draw/drawscene.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <nfauconn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 18:10:19 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/12/16 16:40:41 by nfauconn         ###   ########.fr       */
+/*   Updated: 2022/12/16 19:42:53 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,28 @@ void	drawscene(t_scene *scene, t_img *img)
 //	float	rM = middle->specs.ratio;
 	middle->transform = translation(-0.5, 1, 0.5);
 
-	t_elem	*right = scene->objs->next;
+	t_elem	*right = middle->next;
 //	right->transform = matrix_mult(translation(1.5, 0.5, -0.5), scaling(0.5, 0.5, 0.5));
 	// float	rR = right->specs.ratio;
 	right->transform = matrix_mult(translation(1.5, 0.5, -0.5), scaling(0.5, 0.5, 0.5));
 
-	t_elem	*left = scene->objs->next->next;
+	t_elem	*left = right->next;
 	// float	rL = left->specs.ratio;
 	left->transform = matrix_mult(translation(-1.5, 0.33, -0.75), scaling(0.33, 0.33, 0.33));
+
+	t_elem	*pl = left->next;
+	pl->transform = matrix_mult(pl->transform, rotation_x(M_PI / 2));
+	pl->transform = matrix_mult(pl->transform, translation(0, 0, 3));
+
+	scene->cam->transform = view_transform(create_point(0, 1.5, -5), \
+												create_point(0, 1, 0), \
+												create_vector(0, 1, 0));
+
+	render(scene, img);
+}
+/*
+
+3 SPHERES MODIFIED TO BE PLANES
 
 	t_elem	*floor = left->next;
 	floor->material.specular = 0;
@@ -42,8 +56,5 @@ void	drawscene(t_scene *scene, t_img *img)
 	right_wall->transform = matrix_mult(right_wall->transform, scaling(10.0, 0.01, 10));
 	right_wall->material.specular = 0;
 
- 	scene->cam->transform = view_transform(create_point(0, 1.5, -5), \
-												create_point(0, 1, 0), \
-												create_vector(0, 1, 0));
-	render(scene, img);
-}
+ 
+*/
