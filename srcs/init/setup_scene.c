ede0cd77deb908dbc7163e440d1bb405842d2b0c
no3 <no@no3>
2022-12-16 13:46:05 +0100

    REFACTO BEFORE PLAN restructuration

--- srcs/init/setup_scene.c
+++ srcs/init/setup_scene.c
@@ -6,26 +6,21 @@
 /*   By: fjeiwjifeoh <fjeiwjifeoh@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 14:19:18 by nfauconn          #+#    #+#             */
-/*   Updated: 2022/12/16 13:36:07 by fjeiwjifeoh      ###   ########.fr       */
+/*   Updated: 2022/12/16 13:46:01 by fjeiwjifeoh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "setup.h"
 
-static void	init_paramsetter(t_scene *scene)
+static void	init(t_scene *scene)
 {
+	ft_bzero(scene, sizeof(t_scene));
 	scene->fill_params[ambiantlight] = &set_ambiantlight;
  	scene->fill_params[camera] = &set_camera;
 	scene->fill_params[lightsource] = &set_lights;
 	scene->fill_params[sphere] = &set_sphere;
 	scene->fill_params[plan] = &set_plan;
 	scene->fill_params[cylinder] = &set_cylinder;
-}
-
-static void	init(t_scene *scene)
-{
-	ft_bzero(scene, sizeof(t_scene));
-	init_paramsetter(scene);
 }
 
 void	setup_scene(t_scene *scene, char *file)
